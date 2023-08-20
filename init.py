1f571e0ea8b8d7710015c363e212e9dc5fa24fa2
jacovinus <jacovinus@gmail.com>
2023-08-20 20:51:30 +0200

    fix: more random values

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-
+from random import randrange, choice
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 def initInflux():
@@ -15,16 +15,20 @@ def initInflux():
 write_client = initInflux()
 bucket = "my-bucket"
 write_api = write_client.write_api(write_options=SYNCHRONOUS) 
+country_list = ["Madrid", "Paris", "Rome", "Valencia"]
 
-for value in range(5):
+for value in range(35):
+    randnum = randrange(0, 100, 2)
+    randtime = randrange(1, 5, 1)
+    randcity = choice(country_list)
     point = (
         Point("measurement1")
-        .tag("location", "Madrid")
-        .field("value",value)
+        .tag("location", randcity)
+        .field("value",randnum)
 
     )
     write_api.write(bucket=bucket, org="my-org", record=point)
-    time.sleep(1) # separate the points by 1 second
+    time.sleep(randtime) # separate the points by 1 second
 
 query_api = write_client.query_api()
 
